8d29a021ac3d432a8ca782fc0320edd550ec403a
skhan1987 <skhan@get-licensed.co.uk>
2024-07-27 17:15:30 +0200

    moved files to infra folder
    moved terraform files to infra folder
    added code folder
    added github workflow

--- infra/data.tf
+++ infra/data.tf
@@ -12,6 +12,6 @@ data "aws_iam_policy_document" "instance_assume_role_policy" {
 # create a zip for deployment with terraform
 data "archive_file" "code_dist_zip"{
     type = "zip"
-    source_dir = "${path.module}/${var.code_dist}"
-    output_path = "${path.module}/${var.code_dist}${var.code_dist_version}.zip"
+    source_dir = "../${path.module}/${var.code_dist}"
+    output_path = "../${path.module}/${var.code_dist}${var.code_dist_version}.zip"
 }
