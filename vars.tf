1b24b4d3c6507220d945d98a07f7e0b2685c3d92
skhan1987 <skhan@get-licensed.co.uk>
2024-07-27 09:11:15 +0200

    first commit

---
+++ vars.tf
@@ -0,0 +1,28 @@
+variable "REGION" {
+  default = "us-east-1"
+}
+
+variable "elasticapp" {
+  default = "DockerReactApp"
+}
+variable "beanstalkappenv" {
+  default = "ReactEnvironment"
+}
+variable "solution_stack_name" {
+  default = "64bit Amazon Linux 2023 v4.3.4 running Docker"
+}
+variable "tier" {
+  default = "WebServer"
+}
+ 
+variable "vpc_id" {
+    default = "vpc-0dcbc52a94b403e68"
+}
+variable "public_subnets" {
+    default = ["subnet-06d7adbcb57108a81", 
+                "subnet-01abebfb0a40f6835",
+                "subnet-051a3a40f8d0793f6",
+                "subnet-084b9e66e374c9a03",
+                "subnet-0a0e7b30606ae27ce",
+                "subnet-06713a285c37f58a5"]
+}
